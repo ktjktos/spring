6a49c6af3045f032c904ea0b5c0bde346de444db
potis potisowski <ktjktos@gmail.com>
2026-06-06 11:38:18 +0200

    zad10

--- src/main/java/org/example/controller/RentalController.java
+++ src/main/java/org/example/controller/RentalController.java
@@ -1,12 +1,17 @@
 package org.example.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.dto.RentalRequest;
 import org.example.model.Rental;
 import org.example.model.User;
 import org.example.model.Vehicle;
 import org.example.services.IRentalService;
 import org.example.services.IUserService;
 import org.example.services.IVehicleService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,19 +35,27 @@ public class RentalController {
         return rentalService.findUserRentals(userId);
     }
 
-    @PostMapping("/users/{userId}/rent/{vehicleId}")
-    public Rental rent(@PathVariable String userId, @PathVariable String vehicleId) {
-        User user = userService.findById(userId);
-        Vehicle vehicle = vehicleService.findById(vehicleId).orElse(null);
+    @PostMapping("/rent")
+    public ResponseEntity<Rental> rent(
+            @RequestBody RentalRequest rentalRequest,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        String login = userDetails.getUsername();
+        User user = userService.findByLogin(login).orElseThrow(() -> new IllegalArgumentException("Nie znaleziono takiego usera!"));
+        Vehicle vehicle = vehicleService.findById(rentalRequest.vehicleId())
+                .orElseThrow(() -> new IllegalArgumentException("Nie znaleziono takiego pojazdu!"));
+        Rental rental = rentalService.rentVehicle(user, vehicle);
 
-        if (user != null && vehicle != null) {
-            return rentalService.rentVehicle(user, vehicle);
-        }
-        throw new IllegalArgumentException("Couldn't find user or vehicle.");
+        return ResponseEntity.status(HttpStatus.CREATED).body(rental);
     }
 
-    @PostMapping("/users/{userId}/return")
-    public Rental returnVehicle(@PathVariable String userId) {
-        return rentalService.returnVehicle(userId);
+    @PostMapping("/return")
+    public ResponseEntity<Void> returnVehicle(
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        String login = userDetails.getUsername();
+        User user = userService.findByLogin(login).orElseThrow(() -> new IllegalArgumentException("Nie znaleziono takiego usera!"));
+        rentalService.returnVehicle(user.getId());
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
